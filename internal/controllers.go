08bbb4ebfc94941c548888c756573286a0a329c7
bartmika <bart@mikasoftware.com>
2019-09-07 20:15:33 -0400

    Latest progress.

--- internal/controllers.go
+++ internal/controllers.go
@@ -8,11 +8,10 @@ import (
 	// "fmt"
 
 	// "google.golang.org/grpc"
-	// "github.com/golang/protobuf/ptypes/timestamp"
 
     // "github.com/mikaponics/mikapod-poller/configs"
 	pb "github.com/mikaponics/mikapod-storage/api"
-	// pb2 "github.com/mikaponics/mikapod-soil-reader/api"
+	pb2 "github.com/mikaponics/mikaponics-thing/api"
 )
 
 
@@ -43,13 +42,42 @@ func (app *MikapodRemote) listTimeSeriesData() ([]*TimeSeriesDatum){
 }
 
 func (app *MikapodRemote) uploadTimeSeriesData(data []*TimeSeriesDatum) bool {
-	//TODO: IMPLEMENT CODE HERE ...
-
+    // Convert our `struct` formatted list to be of `protocol buffer`
+	// formatted list which we can use in our `grpc` output.
+	var list []*pb2.TimeSeriesDatum
 	for _, v := range data {
-		log.Printf("TODO - UPLOAD DATA: %v", v)
+        // Create our `protocol buffer` single time-series datum object.
+        ri := &pb2.TimeSeriesDatum{
+			TenantId:   1,
+            SensorId:   v.Instrument,
+            Value:      v.Value,
+			Timestamp:  v.Timestamp,
+        }
+
+		// Attach our single time-series datum object to our `protocol buffer`
+		// list of time-series data.
+        list = append(list, ri)
+    }
+
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	_, err := app.remote.SetTimeSeriesData(ctx, &pb2.SetTimeSeriesDataRequest{
+		Data: list,
+	})
+	if err != nil {
+		log.Fatalf("could not add time-series data to storage: %v", err)
 	}
+
+
+	return err != nil
+}
+
+func (app *MikapodRemote) uploadTimeSeriesDatum(datum *TimeSeriesDatum) bool {
+	log.Printf("TODO - UPLOAD DATA: %v", datum)
 	return false
 }
+
 
 func (app *MikapodRemote) deleteTimeSeriesData(data []*TimeSeriesDatum) {
 	var pks []int64
